63ca5031053115d68012c9d54ecec4cdc1eacb9c
Domene99 <jadomene99@gmail.com>
2023-03-22 17:21:32 -0600

    v1.0.0.0

--- app.py
+++ app.py
@@ -2,29 +2,65 @@ from flask import Flask, request, Response, jsonify
 import json
 import openai
 import os
-import random
-from Color import *
+import PyPDF2
+
 app = Flask(__name__)
 
 MAX_RETRIES = 10
 
-def pickNlpColor(colorname):
-    COLOR_FORMAT_SCHEMA ="""
-    title: Color Formats
-type: object
-properties:
-  hex:
-    type: string
-    pattern: "^#[a-fA-F\\d]{3,6}$"
-  rgb:
-    items:
-      type: number
-  hsl:
-    items:
-      type: number
+def resumeParse(resume):
+    RESUME_SCHEMA ="""
+    "{
+    "full_name": "",
+    "email": "",
+    "phone": "",
+    "address": "",
+    "linkedin": "",
+    "website": "",
+    "objective": "",
+    "summary": "",
+    "education": [
+        {
+            "institution": "",
+            "degree": "",
+            "field_of_study": "",
+            "start_date": "",
+            "end_date": "",
+            "gpa": "",
+            "honors_awards": [],
+            "courses": []
+        }
+    ],
+    "experience": [
+        {
+            "title": "",
+            "company": "",
+            "location": "",
+            "start_date": "",
+            "end_date": "",
+            "description": "",
+            "achievements": [],
+            "technologies": [],
+            "projects": []
+        }
+    ],
+    "skills": {
+        "technical": [],
+        "soft": []
+    },
+    "languages": [],
+    "certifications": [],
+    "volunteer_experience": [],
+    "publications": [],
+    "patents": [],
+    "hobbies_interests": []
+}
+" 
     """
 
-    CONTENT = "Give me the most likely rgb, hex, and hsl values for the phrase or word \" " + colorname  + " \" following the schema \"" + COLOR_FORMAT_SCHEMA + "\". Include no added commentary, explanation, confirmation or preamble in your response. Include only the json response"
+    CONTENT = "Use this schema \" " + RESUME_SCHEMA  + " \" to parse this resume \"" + resume + "\". Include no added commentary, explanation, confirmation or preamble in your response. Include only the json response"
+    if len(CONTENT) // 4 > 2500:
+        return { "choices": [ { "finish_reason" : "length" } ] }
 
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
@@ -35,74 +71,30 @@ properties:
     )
     
     return response
+    # return {"hi": "you"}
 
-# def mockNlpColor(colorname):
-#     return """{
-#   "hex": "#008080",
-#   "rgb": [0, 128, 128],
-#   "hsl": [180, 100, 25]
-# }"""
-
-# def mockNlpColorNotValid(colorname):
-#     return """
-#   "hex": "#008080",
-#   "rgb": [0, 128, 128],
-#   "hsl": [180, 100, 25]
-# }"""
-
-# def mockNlpColorMaybeValid(colorname):
-#     number = random.randrange(0, 10)
-#     print(number)
-#     if number < 1:
-#         return mockNlpColor(colorname)
-#     return mockNlpColorNotValid(colorname)
-
-@app.route('/palette/<hexcolor>', methods=["GET"])
-def palette(hexcolor):
-    hex_color = request.view_args['hexcolor']
-    r, g, b = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    color = Color([r,g,b],"","")
-    complementary = complementaryColor(color)
-    triad = triadicColor(color)
-    split = splitComplementaryColor(color)
-    tetradic = tetradicColor(color)
-    analogous = analogousColor(color)
-    monochromatic = monochromaticColor(color)
-    result = {
-        "color": hexcolor,
-        "complementary": complementary,
-        "triad": triad,
-        "split": split,
-        "tetradic": tetradic,
-        "analogous": analogous,
-        "monochromatic": monochromatic
-        }
-    return jsonify(result), 200
-
-@app.route('/color/<colorname>', methods=["GET"])
-def nlpColorPicker(colorname):
-    color_name = request.view_args['colorname']
-
+@app.route('/resume', methods=["GET"])
+def resume():
+    resume = request.form.get("resume")
+    
     key = os.getenv('OPENAI_KEY')
     openai.api_key = key
 
     response = None
-    retries = 0
-    while retries < MAX_RETRIES and response is None:
+    # retries = 0
+    # while retries < MAX_RETRIES and response is None:
+    while response is None:
         try:
-            # mock_response = json.loads(mockNlpColorMaybeValid(colorname))
-            openai_response = pickNlpColor(colorname)
-            response = json.loads(openai_response["choices"][0]["message"]["content"])
-            
+            openai_response = resumeParse(resume)
+            if openai_response["choices"][0]["finish_reason"] == "length":
+                return "Exceeded maximum size of resume (~8,000 chars or ~1500 words)", 413
+            response = json.loads(openai_response["choices"][0]["message"]["content"])            
         except ValueError:
             response = None
-        retries += 1
     if response is None:
         return "Malformed json response from neural network.", 500
     
     return jsonify(response), 200
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
